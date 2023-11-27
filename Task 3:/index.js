0207dc706ee04d11e12baaa41801508d0ef8c3f6
Abdul Wasea <136252122+awrgb@users.noreply.github.com>
2023-11-27 14:01:51 +0500

    index.js

---
+++ Task 3:/index.js
@@ -0,0 +1,29 @@
+// Scenario 1 - Modify Array with Methods
+const initialArray  = [1, 2, 3, 4, 5];
+console.log("Initial Array", initialArray);
+
+initialArray.push(6, 7); // Add elements to the end
+console.log("After push", initialArray);
+
+initialArray.pop(); // Remove the last element
+console.log("After pop", initialArray);
+
+initialArray.shift(); // Remove the first element
+console.log("After shift", initialArray);
+
+initialArray.unshift(0); // Add elements to the beginning
+console.log("After unshift", initialArray);
+
+// Scenario 2 - Subarray Creation
+const originalArray  = [10, 20, 30, 40, 50];
+console.log("Original Array", originalArray);
+
+// Create a subarray using splice
+const splicedArray  = originalArray.splice(1, 3);
+console.log("Subarray using splice", splicedArray);
+console.log("Original Array after splice", originalArray);
+
+// Create a subarray using slice
+const slicedArray  = originalArray.slice(1, 3);
+console.log("Subarray using slice", slicedArray);
+console.log("Original Array after slice", originalArray);
